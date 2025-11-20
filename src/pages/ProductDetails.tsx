2ffab3542abd3ca04328aa149d7beb6b32fa2f2b
Dev Jeff <arribion@gmail.com>
2025-11-20 07:59:47 +0300

    set ui for product details page

--- src/pages/ProductDetails.tsx
+++ src/pages/ProductDetails.tsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom"
-
+import { IoCartOutline } from "react-icons/io5";
 const ProductDetails = () => {
   return (
     <section className="mt-30 min-h-[50vh] mx-4">
@@ -12,8 +12,57 @@ const ProductDetails = () => {
         </li>
       </ul>
 
+      <hr className="my-4" />
+
+      <div className="flex gap-8">
+        <div>
+          <div className="bg-blue-400 rounded-2xl min-w-[15em] min-h-[40vh]">
+            <img src="" alt="" />
+          </div>
+          <div className="flex gap-4">
+            <div className="bg-blue-500 w-[10em] h-[10em] rounded-2xl">
+              <img src="" alt="" />
+            </div>
+            <div className="bg-blue-500 w-[10em] h-[10em] rounded-2xl">
+              <img src="" alt="" />
+            </div>
+            <div className="bg-blue-500 w-[10em] h-[10em] rounded-2xl">
+              <img src="" alt="" />
+            </div>
+          </div>
+        </div>
+        <div>
+          <h1>Name</h1>
+          <h2>KES 12,000</h2>
           <div>
-              
+            colors
+            <div>
+              <span>RED</span>
+              <span>RED</span>
+              <span>RED</span>
+            </div>
+          </div>
+          <div>
+            <span>S</span>
+            <span>M</span>
+            <span>L</span>
+            <span>XL</span>
+            <span>XXL</span>
+          </div>
+          <hr />
+          <div className="flex gap-4">
+            <button className="bg-blue-700 border rounded-2xl w-[10em] font-bold my-4 ">
+              BUY NOW
+            </button>
+            <button className="flex gap-4 border rounded-2xl w-[10em] font-bold items-center my-4">
+              <IoCartOutline /> ADD TO CART
+            </button>
+          </div>
+          <hr />
+          <p>Description</p>
+        </div>
+
+        <div className="bg-blue-50 w-[30em] h-[30em] rounded-2xl"></div>
       </div>
     </section>
   );
