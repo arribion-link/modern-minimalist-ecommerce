dba44083262b2ebd5b5244a330521b16a6b9e5ff
Dev Jeff <arribion@gmail.com>
2025-11-20 08:07:23 +0300

    added not found images

--- src/pages/util/PageNotFound.tsx
+++ src/pages/util/PageNotFound.tsx
@@ -1,7 +1,8 @@
-
+import nfi from "../../assets/page-not-found.jpg"
 const PageNotFound = () => {
   return (
     <section className="mt-20 mx-4">
+      <img src={nfi} alt="" className="max-w-80" />
       <h1 className='text-5xl'>404 page not Found</h1>
     </section>
   )
